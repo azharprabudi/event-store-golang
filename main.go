0aff42400725d181c4e25acc11f9f03e19f6f53b
azharprabudi <azharprabui@gmail.com>
2018-11-09 10:50:50 +0700

    add example

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"event-store/SimpleEventStore"
+	ses "event-store/simple-event-store"
+	sesHelper "event-store/simple-event-store/helper"
+	"time"
 )
 
+// ExampleUserBalance ...
+type ExampleUserBalance struct {
+	UserID    string     `bson:"userId"`
+	Balance   int64      `bson:"balance"`
+	CreatedAt time.Time  `bson:"createdAt"`
+	UpdatedAt *time.Time `bson:"updatedAt"`
+}
+
 func main() {
-	SimpleEventStore.NewSimpleventStore()
+	db, err := sesHelper.OpenDBConnection("mongodb://root:root123456@ds115022.mlab.com:15022/cqrs", "cqrs")
+	if err != nil {
+		panic(err)
+	}
+	newSes := ses.NewSimpleEventStore(10, "user_balance", db, []string{"userId", "balance", "createdAt", "updatedAt"})
+	newSes.Insert(ExampleUserBalance{
+		UserID:    "8937ca65-2cf9-41fc-b23f-21be09e55938",
+		Balance:   64000,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: nil,
+	})
 }
